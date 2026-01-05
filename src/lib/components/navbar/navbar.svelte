afcbbb8960827652a207213a037bf9425fc2daba
Régis Tremblay Lefrançois <regg00@gmail.com>
2026-01-05 14:14:26 -0500

    added get started button

--- src/lib/components/navbar/navbar.svelte
+++ src/lib/components/navbar/navbar.svelte
@@ -7,17 +7,17 @@
 </script>
 
 <nav class="h-16 border-b border-accent bg-background">
-  <div class="h-full mx-auto flex max-w-(--breakpoint-xl) items-center justify-between px-4 sm:px-6">
+  <div
+    class="h-full mx-auto flex max-w-(--breakpoint-xl) items-center justify-between px-4 sm:px-6"
+  >
     <Logo />
 
     <NavMenu class="hidden md:block" />
 
     <div class="flex items-center gap-3">
       <DarkModeSwitch />
-      <Button variant="outline" class="hidden sm:inline-flex">
-        Sign In
-      </Button>
-      <Button class="hidden xs:inline-flex">Get Started</Button>
+      <Button variant="outline" class="hidden sm:inline-flex">Sign In</Button>
+      <Button class="xs:inline-flex">Get Started</Button>
       <div class="md:hidden">
         <NavigationSheet />
       </div>
