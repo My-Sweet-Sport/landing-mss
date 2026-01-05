1a19a02bdc0d5dfcbc8319af1cd9375be737f9df
Régis Tremblay Lefrançois <regg00@gmail.com>
2026-01-05 13:59:18 -0500

    added footer and hero

---
+++ src/lib/components/hero.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { Badge } from "$lib/components/ui/badge";
+	import { Button } from "$lib/components/ui/button";
+	import { ArrowUpRight, CirclePlay } from "@lucide/svelte";
+</script>
+
+<div class="min-h-[calc(100vh-4rem)] w-full flex items-center justify-center overflow-hidden border-b border-accent">
+	<div class="max-w-(--breakpoint-xl) w-full flex flex-col lg:flex-row mx-auto items-center justify-between gap-y-14 gap-x-10 px-6 py-12 lg:py-0">
+		<div class="max-w-xl">
+			<Badge class="rounded-full py-1 border-none">Just released v1.0.0</Badge>
+			<h1 class="mt-6 max-w-[20ch] text-3xl xs:text-4xl sm:text-5xl lg:text-[2.75rem] xl:text-5xl font-bold leading-[1.2]! tracking-tight">
+				Customized Shadcn UI Blocks & Components
+			</h1>
+			<p class="mt-6 max-w-[60ch] xs:text-lg">
+				Explore a collection of Shadcn UI blocks and components, ready to preview and copy. Streamline your development workflow with easy-to-implement examples.
+			</p>
+			<div class="mt-12 flex flex-col sm:flex-row items-center gap-4">
+				<Button size="lg" class="w-full sm:w-auto rounded-full text-base">
+					Get Started <ArrowUpRight class="h-5! w-5!" />
+				</Button>
+				<Button variant="outline" size="lg" class="w-full sm:w-auto rounded-full text-base shadow-none">
+					<CirclePlay class="h-5! w-5!" /> Watch Demo
+				</Button>
+			</div>
+		</div>
+		<div class="relative lg:max-w-lg xl:max-w-xl w-full bg-accent rounded-xl aspect-square">
+			<img src="/placeholder.svg" alt="" class="absolute inset-0 size-full object-cover rounded-xl" />
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,26 @@
   import Pricing from "$lib/components/pricing.svelte";
   import FAQ from "$lib/components/faq.svelte";
   import Features from "$lib/components/features.svelte";
+  import Footer from "$lib/components/footer.svelte";
+  import Hero from "$lib/components/hero.svelte";
 </script>
 
-<Pricing />
-<FAQ />
+<Hero />
 <Features />
+<FAQ />
+<Pricing />
+<Footer />
+
+<!-- export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <FAQ />
+      <Testimonial />
+      <Pricing />
+      <Footer />
+    </>
+  );
+} -->
