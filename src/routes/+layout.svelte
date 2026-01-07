efeb19a03e3d84bb2fad90a008f0900d52d428f0
Régis Tremblay Lefrançois <regg00@gmail.com>
2026-01-07 14:53:30 -0500

    removed imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,17 +5,13 @@
 
   import "./layout.css";
 
-  import FAQ from "$lib/components/faq.svelte";
-  import Features from "$lib/components/features.svelte";
   import Footer from "$lib/components/footer.svelte";
-  import Hero from "$lib/components/hero.svelte";
+
   import Navbar from "$lib/components/navbar/navbar.svelte";
-  import Pricing from "$lib/components/pricing.svelte";
-  import Testimonial from "$lib/components/testimonials.svelte";
 </script>
 
 <svelte:head><link rel="icon" href={`${base}/favicon-2.ico`} /></svelte:head>
-<ModeWatcher defaultMode={"dark"} />
+<ModeWatcher defaultMode={"system"} />
 <Navbar />
 {@render children()}
 <Footer />
