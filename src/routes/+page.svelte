177d81fd430162c0c7ce736952821ff182516658
Régis Tremblay Lefrançois <regg00@gmail.com>
2026-02-01 14:26:42 -0500

    removed testimonials

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { t } from 'svelte-i18n';
   import FAQ from "$lib/components/faq.svelte";
   import Features from "$lib/components/features.svelte";
   import Hero from "$lib/components/hero.svelte";
@@ -7,15 +8,15 @@
 </script>
 
 <svelte:head>
-  <title>My Sweet Sport</title>
+  <title>{$t('meta.title')}</title>
   <meta
     name="description"
-    content="Learn what is My Sweet Sport, how it works, and how it can help you grow your sport performance."
+    content={$t('meta.description')}
   />
 </svelte:head>
 
 <Hero />
 <Features />
 <FAQ />
-<Testimonial />
+<!-- <Testimonial /> -->
 <Pricing />
